e802b08115fc0e97718df574eb6ec0aa6f15ec7a
Vikhyat Gupta <vikhyat@suraasa.com>
2022-06-22 14:57:01 +0530

    New Device

--- SavePass/settings.py
+++ SavePass/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+"Repo Accessed From A New Device"
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
